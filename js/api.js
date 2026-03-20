b4b98933b84031fb11226b36587b5fcb3cf65766
Dakdan <khowit7@gmail.com>
2026-03-20 14:48:11 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,26 +1,32 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbwml8efHi7YGiLFgBPFJcdVqOFxqEFYvGd6lz-neZIXevaemNaYwXwsPlF91VqfeUlm/exec";
 
-// 🔗 เปลี่ยนเป็น Web App URL ของคุณ
+async function api(action, params = {}) {
+  showLoading(true);
 
-// 🔗 เปลี่ยนเป็น Web App URL ของคุณ
-const API_URL="https://script.google.com/macros/s/AKfycbwml8efHi7YGiLFgBPFJcdVqOFxqEFYvGd6lz-neZIXevaemNaYwXwsPlF91VqfeUlm/exec"
+  const url = new URL(API_URL);
+  url.searchParams.append("action", action);
 
-async function api(action,params={}){
- showLoading(true)
- const url=new URL(API_URL)
- url.searchParams.append('action',action)
- Object.keys(params).forEach(k=>url.searchParams.append(k,params[k]))
- const res=await fetch(url)
- const j=await res.json()
- showLoading(false)
- return j
+  Object.keys(params).forEach(k => {
+    url.searchParams.append(k, params[k]);
+  });
+
+  const res = await fetch(url);
+  const json = await res.json();
+
+  showLoading(false);
+  return json;
 }
 
-async function apiPost(action,data={}){
- showLoading(true)
- const res=await fetch(API_URL,{method:'POST',body:JSON.stringify({action,...data})})
- const j=await res.json()
- showLoading(false)
- return j
+async function apiPost(action, data = {}) {
+  showLoading(true);
+
+  const res = await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({ action, ...data })
+  });
+
+  const json = await res.json();
+
+  showLoading(false);
+  return json;
 }
-
-
